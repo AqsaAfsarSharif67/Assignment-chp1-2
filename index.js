0e69ddd2d6ff167d8966406b3063527312d0e6c0
aqsaafsarsharif67 <sharifaqsa58@gmail.com>
2024-05-21 14:29:13 -0700

    css-assignmentg

---
+++ index.js
@@ -0,0 +1,129 @@
+var greeting = "Hello! Welcome to our website.";
+ 
+ alert(greeting);
+
+  {
+   alert("Error: Please enter a valid password");
+};
+
+alert("Welcome to JS land\n\nHappy coding");
+
+
+
+// let weight = +prompt("enter weight");
+// let time = +prompt("enter time");
+ 
+// if (!(weight > 300 && time < 6)) {
+//     alert("come to our tryout")
+// } else {
+//     alert("Come to our cookout");
+// }
+
+alert("Welcome to JS land");
+alert("Happy coding\n\Prevent this page from creating additional dialogues");
+alert("Hello! I can run JS through my web browser console");
+
+/////////////////CHAPTER ONE END//////////////////////////////////////////
+
+
+///////////////chapter-2/////////////////////////
+let username;
+console.log(username);
+
+var myName = "Your Full Name";
+var myName = "John Doe";
+
+
+
+
+var message;
+
+
+message = "Hello World";
+
+
+alert(message);
+
+
+
+// Save student's bio data in JavaScript variables
+let studentName = "John Doe";
+let studentAge = 20;
+let studentCourse = "Web And Mobile App Developement";
+
+// Display the student's name in an alert box
+alert("Student Name: " + studentName);
+
+// Display the student's age in an alert box
+alert("Student Age: " + studentAge);
+
+// Display the student's course in an alert box
+alert("Student Course: " + studentCourse);
+
+
+
+// Declare a variable called email and assign a string representing your email address
+let email = "abc@example.com";
+
+// Display a message containing the email address in an alert box using string concatenation
+alert(email);
+
+
+
+
+// Declare a variable called book and assign it the value "A smarter way to learn JavaScript"
+let book = "A smarter way to learn JavaScript";
+
+// Display a message in an alert box using the value of the book variable
+alert("I'm trying to learn JavaScript from the book '" + book + "'");
+
+
+// Create a new <p> element
+let paragraph = document.createElement("p");
+
+// Set the text content of the <p> element
+paragraph.textContent = "Yah! I can write my HTML through my JavaScript";
+
+// Append the <p> element to the <body> of the document
+document.body.appendChild(paragraph);
+
+
+
+// Declare a variable called age and assign it your age
+let age = 18;
+
+// Display your age in an alert box
+alert("I'm 18 years old");
+
+
+// var visit = 14;
+// alert("you have visited this site" + visit +  "time");
+
+
+
+var visit = 14;
+alert("you have visited this site " + visit + " time");
+
+
+ // Visitor's name
+ var visitorName = "John Doe";
+
+ // Product title
+ var productTitle = "T-shirt";
+
+ // Quantity of products
+ var quantity = 5;
+
+ // Construct the message
+ var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+
+ // Display the message in an alert box
+ alert(message);
+
+
+
+// Display the string in an alert
+alert(myString);
+
+// Display the string in the browser
+document.write(myString);
